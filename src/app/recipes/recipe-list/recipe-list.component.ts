0a89a7e93ccb629387a5b4b22ba9b0470e873785
NikhilAdsul <nikhiladsul@gmail.com>
2017-03-20 19:30:32 +0530

    current status in project

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit, EventEmitter, Output} from '@angular/core';
+import {Recipe} from "../recipe";
+import{ RecipeItemComponent} from './recipe-item.component'
+
+
+@Component({
+  selector: 'rb-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[]=[];
+    /*new Recipe('Schnitzel', 'Very tasty', 'http://images.derberater.de/files/imagecache/456xXXX_berater/berater/slides/WienerSchnitzel.jpg', []),
+    new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])];*/
+
+  @Output() recipeSelected=new EventEmitter<Recipe>();
+
+  recipe= new Recipe('Dummy','Dummy','http://thumbs1.ebaystatic.com/d/l225/m/mM5u2kVAYbs3IRlwPfl6uOA.jpg');
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelected(recipe:Recipe)
+  {
+    this.recipeSelected.emit(recipe);
+  }
+
+
+
+}
